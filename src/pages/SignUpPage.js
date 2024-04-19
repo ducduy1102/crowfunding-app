0141b43a7a9efaceb3f2b2be6f7557c7e9a6da7a
ducduy1102 <ducduy1110u@gmail.com>
2024-04-19 16:34:17 +0700

    SignUp #1

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import LayoutAuthentication from "../layout/LayoutAuthentication";
+
+const SignUpPage = () => {
+  return (
+    <LayoutAuthentication heading="SignUp">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates,
+      natus cumque deleniti sapiente, modi nulla pariatur recusandae reiciendis
+      aspernatur tempore ratione. Laboriosam, possimus nemo! Repudiandae dolorem
+      in suscipit delectus quam?
+    </LayoutAuthentication>
+  );
+};
+
+export default SignUpPage;
