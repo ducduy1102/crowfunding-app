bb66f822078ec6f4f2dc4bfdb32d3d4eb659d63e
ducduy1102 <ducduy1110u@gmail.com>
2024-04-22 17:13:32 +0700

    UI Campaign Page

--- src/modules/campaign/CampaignGrid.js
+++ src/modules/campaign/CampaignGrid.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ProptTypes from "prop-types";
 
-const CampaignGrid = ({ children }) => {
+const CampaignGrid = ({ children, type = "default" }) => {
+  if (type !== "default")
+    return <div className="grid grid-cols-1 gap-y-10">{children}</div>;
   return <div className="grid grid-cols-4 gap-x-7">{children}</div>;
 };
 
 CampaignGrid.propTypes = {
   children: ProptTypes.node,
+  type: ProptTypes.string,
 };
 
 export default CampaignGrid;
